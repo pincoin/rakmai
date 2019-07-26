f64eedabd6a2f18a9f06126ed597ef2aa45c0cdf
pincoin <pincoins@gmail.com>
2019-07-26 12:50:36 +0900

    - cache bug fix

--- shop/templatetags/shop_tags.py
+++ shop/templatetags/shop_tags.py
@@ -138,13 +138,13 @@ def get_category_leaf(store_code, pg=False):
                               .filter(store__code=store_code, level__gt=0) \
                               .order_by('-discount_rate', '-title'))
 
-        cache.set(cache_key, categories, cache_time)
-
         # 아프리카TV 별풍선 = 22
         index = next((i for i, item in enumerate(categories) if item.id == 22), -1)
 
         if index > -1:
             categories[index].title = '아프리카별풍선'
             categories.insert(0, categories.pop(index))
+
+        cache.set(cache_key, categories, cache_time)
 
     return categories
