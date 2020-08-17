7a9988a0dccc5aef0e748863e27bde9ebd740987
jonghwa <pincoins@gmail.com>
2020-08-17 18:05:56 +0900

    disallowed email

--- member/settings.py
+++ member/settings.py
@@ -14,3 +14,12 @@ DISALLOWED_EMAIL_DOMAIN = getattr(settings, 'DISALLOWED_EMAIL_DOMAIN', (
     'qq.com', '163.com', '126.com', '188.com', 'yeah.net', 'sina.com', 'hotmail.com', 'live.com',
     'rael.cc', 'moakt.cc', 'grr.la', 'guerrillamail.com',
 ))
+
+ALLOWED_EMAIL_DOMAIN = getattr(settings, 'ALLOWED_EMAIL_DOMAIN', (
+    'gmail.com', 'naver.com', 'hanmail.net', 'nate.com', 'daum.net', 'jr.naver.com',
+    'hanmir.com', 'dreamwiz.com', 'paran.com', 'kakao.com', 'lycos.co.kr', 'sayclub.com', 'empal.com',
+    'icloud.com', 'korea.com', 'nexon.com', 'freechal.com', 'netian.com', 'cyworld.com', 'nexonclub.com', 'hanafos.com',
+    'empas.com', 'chol.com',
+
+    # yahoo.co.kr yahoo.com hotmail.com live.com msn.com outlook.com me.com
+))
