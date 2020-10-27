8792995ae8ff650204431d082deda367bf9e825f
jonghwa <pincoins@gmail.com>
2020-10-27 09:31:55 +0900

    50 years old women ban

--- member/views.py
+++ member/views.py
@@ -540,6 +540,19 @@ class IamportSmsCallbackView(StoreContextMixin, HostContextMixin, views.APIView)
                             }),
                                 status=status.HTTP_400_BAD_REQUEST)
 
+                        # > 50 years old + women + joined within 90 minutes
+                        if now().date() - datetime.strptime(log.date_of_birth, '%Y%m%d').date() \
+                                > timedelta(days=365 * 50) \
+                                and log.gender == 0 \
+                                and now() - profile.user.date_joined < timedelta(days=90):
+                            return Response(data=json.dumps({
+                                'code': 400,
+                                'message': str(
+                                    _(
+                                        'Person aged over 50 can verify your account during 90 days after joined.'))
+                            }),
+                                status=status.HTTP_400_BAD_REQUEST)
+
                         if not banned:
                             profile.phone = log.cellphone
 
