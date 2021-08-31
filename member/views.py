fc058cd766160d456015bb808b39c27e9c401d4d
jonghwa <pincoins@gmail.com>
2021-08-31 23:11:07 +0900

    mvno block

--- member/views.py
+++ member/views.py
@@ -576,6 +576,17 @@ class IamportSmsCallbackView(StoreContextMixin, HostContextMixin, views.APIView)
                             }),
                                 status=status.HTTP_400_BAD_REQUEST)
 
+                        # MVNO + women
+                        if 'MVNO' in log.telecom and log.gender == 0 \
+                                and now() - profile.user.date_joined < timedelta(hours=48):
+                            return Response(data=json.dumps({
+                                'code': 400,
+                                'message': str(
+                                    _(
+                                        'MVNO user can verify your account during 48 hours after joined.'))
+                            }),
+                                status=status.HTTP_400_BAD_REQUEST)
+
                         if not banned:
                             profile.phone = log.cellphone
 
