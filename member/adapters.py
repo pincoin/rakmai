65efdf3e1d7885c9013130d89887f0d2d4bdd357
jonghwa <pincoins@gmail.com>
2021-09-09 23:29:55 +0900

    disable sign up

--- member/adapters.py
+++ member/adapters.py
@@ -42,3 +42,12 @@ class MyAccountAdapter(DefaultAccountAdapter):
             return reverse('card:home', args=('default',))
         else:
             return reverse('shop:home', args=('default',))
+
+    def is_open_for_signup(self, request):
+        """
+        Checks whether or not the site is open for signups.
+
+        Next to simply returning True/False you can also intervene the
+        regular flow by raising an ImmediateHttpResponse
+        """
+        return False
