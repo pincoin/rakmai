e86fa455d7295032a6f96f61d12dd7ecf2dd408d
jonghwa <pincoins@gmail.com>
2022-05-05 01:01:06 +0900

    usd

--- shop/settings.py
+++ shop/settings.py
@@ -45,7 +45,7 @@ CURRENCY_RATE = getattr(settings, 'CURRENCY_RATE', {
     },
     'USD': {
         'locale': 'en_US.UTF-8',
-        'rate': Decimal(0.00101000),  # 0.00106000
+        'rate': Decimal(0.00105000),  # 0.00106000
     },
 })
 
