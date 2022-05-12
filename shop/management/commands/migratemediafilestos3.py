a7982f89275eeac634aafa786b54674fd810a41b
jonghwa <pincoins@gmail.com>
2022-05-12 22:13:42 +0900

    upload media files with content type header

--- shop/management/commands/migratemediafilestos3.py
+++ shop/management/commands/migratemediafilestos3.py
@@ -1,3 +1,4 @@
+import mimetypes
 import os
 
 import boto3
@@ -17,12 +18,26 @@ class Command(BaseCommand):
 
         media_root_parent = os.path.join(os.path.dirname(os.path.dirname(settings.MEDIA_ROOT)), '', '')
 
+        content_types = mimetypes.types_map
+        content_types.update(
+            {
+                '.tgz': 'application/x-tar-gz',
+            }
+        )
+
         for root, dirs, files in os.walk(settings.MEDIA_ROOT):
             for file in files:
                 full_path = os.path.join(root, file)
                 upload_path = full_path.split(media_root_parent)[-1]
                 try:
-                    s3_client.upload_file(full_path, settings.AWS_STORAGE_BUCKET_NAME, upload_path)
+                    s3_client.upload_file(full_path,
+                                          settings.AWS_STORAGE_BUCKET_NAME,
+                                          upload_path,
+                                          ExtraArgs={'Metadata': {
+                                              'ContentType': mimetypes.types_map[
+                                                  str(os.path.splitext(upload_path)[1]).lower()
+                                              ],
+                                          }})
                     self.stdout.write(self.style.SUCCESS(upload_path))
                 except ClientError as e:
                     self.stdout.write(self.style.ERROR(upload_path))
