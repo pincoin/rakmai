93166cefaae9bdd98c2a2d327e3492b43a6b4827
jonghwa <pincoins@gmail.com>
2022-05-12 19:25:27 +0900

    media root

--- shop/management/commands/migratemediafilestos3.py
+++ shop/management/commands/migratemediafilestos3.py
@@ -8,7 +8,6 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # create session to s3
         session = boto3.session.Session()
         s3_client = session.client(
             service_name='s3',
@@ -16,10 +15,9 @@ class Command(BaseCommand):
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
         )
 
-        media_root = getattr(settings, 'MEDIA_ROOT', None)
-        media_root_parent = os.path.join(os.path.dirname(os.path.dirname(media_root)), '', '')
+        media_root_parent = os.path.join(os.path.dirname(os.path.dirname(settings.MEDIA_ROOT)), '', '')
 
-        for root, dirs, files in os.walk(media_root):
+        for root, dirs, files in os.walk(settings.MEDIA_ROOT):
             for file in files:
                 full_path = os.path.join(root, file)
                 upload_path = full_path.split(media_root_parent)[-1]
